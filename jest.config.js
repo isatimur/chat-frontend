c61a2d334fd7b864453f1f23538be4d5e824f2b3
Alexander Anpleenko <avanpleenko@gmail.com>
2022-10-09 22:25:37 +0300

    feat: add test-library-react

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/tests/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/fileMock.js',
+  },
 };
